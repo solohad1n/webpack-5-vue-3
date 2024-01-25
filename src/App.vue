d7d497f71843b994802f182429b587d98000b8cc
solohad1n <fox663399@gmail.com>
2024-01-25 16:27:58 +0600

    init

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+      <h1>Minimal Vue Webpack</h1>
+      <div>
+          <test/>
+      </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'; 
+import test from './components/test.vue';
+
+export default defineComponent({
+components: { test },
+  name: 'App'
+});
+
+</script>
+
+<style scoped>
+  h1 {
+      width: 100%;
+      text-align: center;
+      margin-top: 10px;
+  }
+
+  img {
+      display: block;
+      margin: auto;
+  }
+</style>
---
+++ src/components/test.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div>
+      <img src="../static/home.png" width="50" height="50"/>
+    </div>
+    <div>
+      {{ text }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+data () {
+  return{
+    text: '123'
+  }
+}
+}
+</script>
+
+<style>
+
+</style>
